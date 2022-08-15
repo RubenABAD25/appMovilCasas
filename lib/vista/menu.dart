9b0d0d97b66da386255a5a8e9cafaeb86d88656a
Ruben Abad <rabado@est.ups.edu.ec>
2022-08-15 12:05:17 -0500

    loginPageView

---
+++ lib/vista/menu.dart
@@ -0,0 +1,13 @@
+class menuPageView extends StatefulWidget {
+  const menuPageView({Key? key}) : super(key: key);
+
+  @override
+  State<menuPageView> createState() => _menuPageViewState();
+}
+
+class _menuPageViewState extends State<menuPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
